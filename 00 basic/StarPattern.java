90c8a38f63a59fb94a4b816e8ab7ac76b03bd3e0
hiteshwagh001 <hiteshswaagh001@gmail.com>
2025-01-23 19:01:24 +0530

    add

--- 00 basic/StarPattern.java
+++ 00 basic/StarPattern.java
@@ -1,8 +1,8 @@
-class Patterns{
+class Patterns {
 
     public void block(int n) {
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -11,9 +11,9 @@ class Patterns{
         System.out.println("\n\n");
     }
 
-    public void leftTriangle(int n){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
+    public void leftTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -22,21 +22,10 @@ class Patterns{
 
     }
 
-
-    void leftNumberTriangle1(int n){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print(j+1);
-            }
-            System.out.println();
-        }
-        System.out.println("\n\n");
-
-    }
-    void leftNumberTriangle2(int n){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print(i+1);
+    void leftNumberTriangle1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + 1);
             }
             System.out.println();
         }
@@ -44,9 +33,20 @@ class Patterns{
 
     }
 
-    void inverseLeftTraingle(int n){
-        for(int i=0;i<n;i++){
-            for(int j=n;j>i;j--){
+    void leftNumberTriangle2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(i + 1);
+            }
+            System.out.println();
+        }
+        System.out.println("\n\n");
+
+    }
+
+    void inverseLeftTraingle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
                 System.out.print("*");
             }
             System.out.println();
@@ -54,38 +54,62 @@ class Patterns{
         System.out.println("\n\n");
     }
 
-    void inverseLeftNumberTriangle(int n){
-        for(int i=0;i<n;i++){
-            for(int j=n;j>i;j--){
-                System.out.print(n-j+1);
+    void inverseLeftNumberTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(n - j + 1);
             }
             System.out.println();
         }
         System.out.println("\n\n");
     }
 
-    void pyramid(int n){
-        for(int i=0;i<n;i++){
-            for(int j=n;j>i;j--){
+    void pyramid(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
                 System.out.print(" ");
             }
-            for(int j=0;j<=i;j++){
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
-            for(int j=0;j<i;j++){
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+        System.out.println("\n\n");
+
     }
 
+    void inversePyramid(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n\n");
+    }
+
+    void upperAndLowerPyramid(int n) {
+        for (int i = 0; i < n * 2 - 1; i++) {
+        }
+    }
 
 }
-class StarPattern{
-    public static void main(String[] args) {
-        int n=5;
 
-        Patterns obj=new Patterns();
+class StarPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        Patterns obj = new Patterns();
         obj.block(n);
         obj.leftTriangle(n);
         obj.leftNumberTriangle1(n);
@@ -93,5 +117,6 @@ class StarPattern{
         obj.inverseLeftTraingle(n);
         obj.inverseLeftNumberTriangle(n);
         obj.pyramid(n);
+        obj.inversePyramid(n);
     }
 }
